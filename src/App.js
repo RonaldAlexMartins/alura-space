c5c37496c24ca6acb72916bc242e83b37e75e38e
Ronald Martins <ronny_nascimento@hotmail.com>
2023-05-15 18:18:44 -0300

    Projeto inicial

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import PaginaInicial from "./paginas/PaginaInicial";
+import './styles/estilosGlobais.scss'
+
+export default function App() {
+  return <PaginaInicial/>
+}
---
+++ src/paginas/PaginaInicial/index.jsx
@@ -0,0 +1,12 @@
+import Cabecalho from "../../componentes/cabecalho";
+import Menu from "../../componentes/menu";
+
+export default function PaginaInicial(){
+  return(
+    <>
+    <Cabecalho/>
+    <Menu/>
+    </>
+    
+  )
+}
