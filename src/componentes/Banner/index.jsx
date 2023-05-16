ff2eb5b0d3691fa48df8795b86c0a27e27ca16df
Ronald Martins <ronny_nascimento@hotmail.com>
2023-05-15 23:48:40 -0300

    Criando Variaveis SCSS e Aplicando

--- src/componentes/Banner/index.jsx
+++ src/componentes/Banner/index.jsx
@@ -4,7 +4,7 @@ import styles from "./Banner.module.scss";
 
 export default function Banner() {
   return (
-    <div className={styles.principal}>
+    <div className={styles.banner}>
       <h1>A Galeria mais completa do espaço</h1>
       <img src={banner} alt="A Imagem da terra vista do espaço" />
     </div>
