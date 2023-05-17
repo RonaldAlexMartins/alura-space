e18c5f198b55844bcd09abcd7a6214b4f6c72afa
Ronald Martins <ronny_nascimento@hotmail.com>
2023-05-16 22:18:06 -0300

    correção do diretorio de fotos

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -2,27 +2,29 @@ import React from "react";
 import styles from "./Galeria.module.scss";
 import Tags from "../Tags";
 import fotos from "./fotos.json";
+import open from "./open.png";
+import favorito from "./favorito.png";
 
 export default function Galeria() {
   return (
     <section className={styles.galeria}>
       <h2>Navegue pela Galeria</h2>
       <Tags />
-      <ul className={styles.galeria__lista}>
-        {fotos.map((foto) => {
+      <ul className={styles.galeria__cards}>
+        {fotos.map((fotos) => {
           return (
-            <li key={foto.id} className={styles.galeria__card}>
+            <li key={fotos.id} className={styles.galeria__card}>
               <img
                 className={styles.galeria__imagem}
-                src={foto.imagem}
-                alt={foto.titulo}
+                src={fotos.imagem}
+                alt={fotos.titulo}
               />
-              <p className={styles.galeria__descricao}>{foto.titulo}</p>
+              <p className={styles.galeria__descricao}>{fotos.titulo}</p>
               <div>
-                <p>{foto.creditos}</p>
+                <p>{fotos.creditos}</p>
                 <span>
-                  <img src="" alt="Ícone coração de curtir" />
-                  <img src="" alt="Ícone de abrir modal" />
+                  <img src={favorito} alt="Ícone coração de curtir" />
+                  <img src={open} alt="Ícone de abrir modal" />
                 </span>
               </div>
             </li>
