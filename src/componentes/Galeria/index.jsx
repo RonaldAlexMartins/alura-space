b4fb243c3adbca702c0c9a4ef660f19da7b83291
Ronald Martins <ronny_nascimento@hotmail.com>
2023-05-16 21:38:18 -0300

    Incluido Imagens no Componente Galeria

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -1,2 +1,34 @@
-import React from 'react'
-import styles from './Galeria.module.scss'
+import React from "react";
+import styles from "./Galeria.module.scss";
+import Tags from "../Tags";
+import fotos from "./fotos.json";
+
+export default function Galeria() {
+  return (
+    <section className={styles.galeria}>
+      <h2>Navegue pela Galeria</h2>
+      <Tags />
+      <ul className={styles.galeria__lista}>
+        {fotos.map((foto) => {
+          return (
+            <li key={foto.id} className={styles.galeria__card}>
+              <img
+                className={styles.galeria__imagem}
+                src={foto.imagem}
+                alt={foto.titulo}
+              />
+              <p className={styles.galeria__descricao}>{foto.titulo}</p>
+              <div>
+                <p>{foto.creditos}</p>
+                <span>
+                  <img src="" alt="Ícone coração de curtir" />
+                  <img src="" alt="Ícone de abrir modal" />
+                </span>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
